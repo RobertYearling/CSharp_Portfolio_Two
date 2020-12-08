a5111065a05825fa4c4969b2a00a987fc1d5c61a
Robert Yearling <laserguy@mac.com>
2020-12-07 23:07:06 -0600

    Final Files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun.Controllers     //be sure to use your own project's namespace!
+{
+    public class HomeController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("projects")]
+        public ViewResult Projects()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public ViewResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult Process(string name, string email, string message)
+        {
+            return View("Index");
+        }
+    }
+}
